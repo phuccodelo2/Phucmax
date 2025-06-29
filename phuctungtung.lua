e0ee73d1b7206c3c992574f787167c00a9a35c47
main.lua <nhuyenthanhphuc700@gmail.com>
2025-06-29 16:39:20 +0000

    Update phuctungtung.lua

--- phuctungtung.lua
+++ phuctungtung.lua
@@ -21,9 +21,9 @@ end)
 
 
 local doorPositions = {
-	Vector3.new(-469, 25, -102), Vector3.new(-468, 25, 8), Vector3.new(-467, 25, 112),
-	Vector3.new(-466, 25, 220), Vector3.new(-355, 25, 219), Vector3.new(-354, 25, 112),
-	Vector3.new(-353, 25, 4), Vector3.new(-353, 25, -100)
+	Vector3.new(-469, 40, -102), Vector3.new(-468, 40, 8), Vector3.new(-467, 40, 112),
+	Vector3.new(-466, 40, 220), Vector3.new(-355, 40, 219), Vector3.new(-354, 40, 112),
+	Vector3.new(-353, 40, 4), Vector3.new(-353, 40, -100)
 }
 
 local function getNearestDoor()
